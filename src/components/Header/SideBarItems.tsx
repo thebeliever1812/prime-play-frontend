aa3b27a6a45c48fc779f4d2aafa8fb9564b3d445
thebeliever1812 <basirahmadmalik@gmail.com>
2026-01-25 15:56:23 +0530

    enhance Navbar and Sidebar components: improve layout and add icons to sidebar items

--- src/components/Header/SideBarItems.tsx
+++ src/components/Header/SideBarItems.tsx
@@ -2,6 +2,7 @@
 import Link from 'next/link'
 import React from 'react'
 import { usePathname } from 'next/navigation'
+import { History, ListVideo, MessageSquare, Settings, ThumbsUp, Video } from 'lucide-react'
 
 interface SidebarItemsProps {
     setShowMenu: React.Dispatch<React.SetStateAction<boolean>>
@@ -10,31 +11,13 @@ interface SidebarItemsProps {
 const SidebarItems: React.FC<SidebarItemsProps> = ({ setShowMenu }) => {
 
     const sidebarItems = [
-        {
-            label: "Watch History",
-            path: "/watch-history"
-        },
-        {
-            label: "Liked Videos",
-            path: "/liked-videos"
-        },
-        {
-            label: "My Playlist",
-            path: "/my-playlist"
-        },
-        {
-            label: "My Videos",
-            path: "/my-videos"
-        },
-        {
-            label: "Settings",
-            path: "/settings"
-        },
-        {
-            label: "Send Feedback",
-            path: "/send-feedback"
-        },
-    ]
+        { label: 'Watch History', path: '/watch-history', icon: History },
+        { label: 'Liked Videos', path: '/liked-videos', icon: ThumbsUp },
+        { label: 'My Playlist', path: '/my-playlist', icon: ListVideo },
+        { label: 'My Videos', path: '/my-videos', icon: Video },
+        { label: 'Settings', path: '/settings', icon: Settings },
+        { label: 'Send Feedback', path: '/send-feedback', icon: MessageSquare },
+    ];
 
     const pathname = usePathname()
 
@@ -43,8 +26,14 @@ const SidebarItems: React.FC<SidebarItemsProps> = ({ setShowMenu }) => {
             {
                 sidebarItems.map((item, index) => {
                     const isActive = item.path === pathname
-                    return <li key={index} className={`w-full rounded-lg cursor-pointer hover:bg-[#22C55E] duration-200 hover:shadow-2xs active:bg-white select-none ${isActive ? "bg-[#22C55E]" : "bg-[#BBF7D0]"}`} onClick={() => setShowMenu(false)}>
-                        <Link href={item.path} className='w-full block px-4 py-2 rounded-lg'>{item.label}</Link>
+                    return <li key={index} className={`w-full rounded-lg cursor-pointer hover:bg-[#d8fde5] hover:text-[#1cbd55] hover:shadow-2xs ease-in active:bg-white select-none ${isActive ? "bg-[#d8fde5] text-[#1cbd55]" : "bg-white"}`} onClick={() => setShowMenu(false)}>
+                        
+                        <Link href={item.path} className='w-full block rounded-lg'>
+                            <div className='w-full px-4 py-2 rounded-lg flex justify-start items-center gap-2'>
+                                <item.icon className="h-5 w-5 shrink-0" />
+                                {item.label}
+                            </div>
+                        </Link>
                     </li>
                 })
             }
--- src/components/Header/Sidebar.tsx
+++ src/components/Header/Sidebar.tsx
@@ -1,6 +1,10 @@
+"use client"
+
 import React from 'react'
 import { SidebarItems } from './index';
 import { X } from 'lucide-react';
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 
 interface SidebarProps {
     showMenu: boolean;
@@ -8,12 +12,19 @@ interface SidebarProps {
 }
 
 const Sidebar: React.FC<SidebarProps> = ({ showMenu, setShowMenu }) => {
+    const router = useRouter()
     return (
         <aside className=''>
             <div className={`fixed w-full h-screen bg-black/40 backdrop-blur-[2px] top-0 z-50 ${showMenu ? 'block' : 'hidden'}`} onClick={() => setShowMenu(false)}>
             </div>
             <div className={`fixed bg-white w-full max-w-80 h-screen left-0 top-0 z-[60] px-6 py-3 overflow-y-auto transition-transform duration-500 ease-out ${showMenu ? "translate-x-0" : "-translate-x-full"}`}>
-                <X size={28} className='mt-1 mb-3 cursor-pointer' onClick={() => setShowMenu(false)}/>
+                <div className='flex items-center justify-between mb-3'>
+                    <div className='w-[25px] sm:w-[30px] aspect-square relative cursor-pointer' onClick={() => router.push("/")}>
+                        <Image src={"/logo.png"} alt='Prime play logo' fill />
+                    </div>
+                    <X size={28} className='mt-1 mb-3 cursor-pointer' onClick={() => setShowMenu(false)} />
+                </div>
+
                 <SidebarItems setShowMenu={setShowMenu} />
             </div>
         </aside>
