7793566357c265971e0e8ada7e08a23aa784f1de
thebeliever1812 <basirahmadmalik@gmail.com>
2026-01-25 16:59:56 +0530

    adjusted the padding of sidebar

--- src/components/Header/Sidebar.tsx
+++ src/components/Header/Sidebar.tsx
@@ -18,11 +18,11 @@ const Sidebar: React.FC<SidebarProps> = ({ showMenu, setShowMenu }) => {
             <div className={`fixed w-full h-screen bg-black/40 backdrop-blur-[2px] top-0 z-50 ${showMenu ? 'block' : 'hidden'}`} onClick={() => setShowMenu(false)}>
             </div>
             <div className={`fixed bg-white w-full max-w-80 h-screen left-0 top-0 z-[60] px-2 py-3 overflow-y-auto transition-transform duration-500 ease-out ${showMenu ? "translate-x-0" : "-translate-x-full"}`}>
-                <div className='flex items-center justify-between pb-3 px-4 border-b border-gray-300'>
+                <div className='flex items-center justify-between pb-4 pt-1 px-4 border-b border-gray-300'>
                     <div className='w-6 sm:w-7 aspect-square relative cursor-pointer' onClick={() => router.push("/")}>
                         <Image src={"/logo.png"} alt='Prime play logo' fill />
                     </div>
-                    <X className='w-6 h-6 mt-1 mb-3 cursor-pointer' onClick={() => setShowMenu(false)} />
+                    <X className='w-6 h-6 cursor-pointer' onClick={() => setShowMenu(false)} />
                 </div>
 
                 <SidebarItems setShowMenu={setShowMenu} />
